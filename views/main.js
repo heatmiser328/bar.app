c9a2974facc52e453b3f307752aff69395a714e0
Jeff Capuano <jcapuano328@gmail.com>
2015-07-07 16:57:25 -0400

    add image to main view

---
+++ views/main.js
@@ -0,0 +1,39 @@
+var select = require('../views/select.js');
+var Battle = require('../views/battle.js');
+var Current = require('../core/current.js');
+var config = require('../config.js');
+var log = require('../core/log.js');
+
+module.exports = {
+	run: function() {
+    	var page = tabris.create("Page", {
+        	title: "Battles of the American Revolution Assistant",
+            image: "images/bar.png",
+            background: config.background,
+            textColor: config.textColor,
+            topLevel: true
+		});
+        var labelHeader = tabris.create("TextView", {
+        	text: "Welcome! Please select a battle to get started.",
+        	font: "24px",
+            background: config.background,
+            textColor: config.textColor,
+            layoutData: {left: 15, top: 20}
+		}).appendTo(page);
+        
+        tabris.create("ImageView", {
+        	layoutData: {left: 0, right: 0, top: [labelHeader,0], bottom: 0},
+            image: 'images/fifeanddrum.jpg',
+            scaleMode: "fit"
+		}).appendTo(page);
+        
+		select.init();
+        page.open();
+        
+        var current = Current.get();
+        if (current != null) {
+        	log.debug('Loading current ' + current.battle);
+        	Battle.show(current.battle);
+        }
+    }
+};
